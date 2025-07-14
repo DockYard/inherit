737a63dd0ccb1e0afce3da6d6795655cc7ffe66b
Brian Cardarella <bcardarella@gmail.com>
2025-07-14 03:55:14 -0400

    Test case for deeper inheritance

--- test/inherit_test.exs
+++ test/inherit_test.exs
@@ -17,8 +17,17 @@ defmodule InheritTest do
     ]
   end
 
+  defmodule Baz do
+    use Bar, [
+      b: 3
+    ]
+  end
+
   test "Bar inherits fields and public functions from Foo" do
     bar = %Bar{}
     assert Bar.add(bar.a, bar.b) == 3
+
+    baz = %Baz{}
+    assert Baz.add(baz.a, baz.b) == 4
   end
 end
